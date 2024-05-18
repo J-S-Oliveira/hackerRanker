2a6700aca5466b3ca8e3fb79f54905af17fa70fd
J.S Oliveira <70406366+J-S-Oliveira@users.noreply.github.com>
2024-05-18 11:04:27 -0300

    summary
    Task
    The provided code stub reads two integers,  and , from STDIN.
    
    Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  /

---
+++ ArithmeticOperators.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+
+#The first line contains the sum of the two numbers.
+first = a + b
+print(first)
+#The second line contains the difference of the two numbers (first - second).
+second = a-b
+print (second)
+#The third line contains the product of the two numbers.
+third = a * b
+print(third)
---
+++ PythonDivision.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+
+#The result of the integer division .
+c = int(a/b)
+print(c)
+#The result of the float division 
+d = float(a/b)
+print(d)
